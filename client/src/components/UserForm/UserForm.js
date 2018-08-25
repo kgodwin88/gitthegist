2145a73b03002b9aa1bd7bceb794470dacc6579f
kgodwin88 <kgodwin1988@aol.com>
2018-08-24 22:54:51 -0400

    convert

--- client/src/components/UserForm/UserForm.js
+++ client/src/components/UserForm/UserForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
 const allowed = /^([a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$)/;
+let interestArray = [];
+let knowledgeArray = [];
 class UserForm extends Component {
   state = {
     first_name: "",
@@ -12,12 +14,36 @@ class UserForm extends Component {
     image: "",
     password2: ""
   };
+  onKeyPress = event =>{
+    if (event.which === 13 /* Enter */) {
+      event.preventDefault();
+    }
+    
+  }
   handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({
       [name]: value
     });
   };
+  addInterest = event =>{
+    event.preventDefault();
+    if (event.which === 13 /* Enter */ && this.state.interest) {
+      interestArray.push(this.state.interest.trim())
+      this.setState({interest: ''})
+      window.M.toast({html: "Item added to Interest", classes: 'cyan'})
+      console.log(interestArray)
+    }  
+  }
+  addKnowledge = event =>{
+    event.preventDefault();
+    if (event.which === 13 /* Enter */ && this.state.knowledge) {
+      knowledgeArray.push(this.state.knowledge.trim())
+      window.M.toast({html: "Item added to Knowledge", classes: 'cyan'})
+      this.setState({knowledge: ''})
+      console.log(knowledgeArray)
+    }
+  }
   handleFormSubmit = event => {
     event.preventDefault();
     if (!this.state.first_name && !this.state.last_name) {
@@ -31,7 +57,7 @@ class UserForm extends Component {
       window.M.toast({ html: "Please enter a password", classes: "cyan" });
     } else if (!this.state.password2) {
       window.M.toast({ html: "Please Confirm Your Password", classes: "cyan" });
-    } else if (!this.state.interest) {
+    } else if (!this.state.interest && (interestArray.length===0)) {
       window.M.toast({ html: "Please enter some Interest", classes: "cyan" });
     } else if (this.state.password.length < 6) {
       window.M.toast({
@@ -41,13 +67,19 @@ class UserForm extends Component {
     } else if (!allowed.test(this.state.email)) {
       window.M.toast({ html: "Please Enter a valid Email", classes: "cyan" });
     } else {
+      if(this.state.interest){
+        interestArray.push(this.state.interest)
+      }
+      if(this.state.knowledge){
+        knowledgeArray.push(this.state.knowledge)
+      }
       const new_user = {};
-      new_user.name = `${this.state.first_name} ${this.state.last_name}`;
-      new_user.email = this.state.email;
-      new_user.password = this.state.password;
-      new_user.password2 = this.state.password2;
-      new_user.knowledge = this.state.knowledge.split(", ");
-      new_user.interests = this.state.interest.split(", ");
+      new_user.name = `${this.state.first_name.trim()} ${this.state.last_name.trim()}`;
+      new_user.email = this.state.email.trim();
+      new_user.password = this.state.password.trim();
+      new_user.password2 = this.state.password2.trim();
+      new_user.knowledge = knowledgeArray
+      new_user.interests = interestArray
       new_user.image = this.state.image;
 
       API.registerUser(new_user).then(success => {
@@ -77,7 +109,7 @@ class UserForm extends Component {
   render() {
     return (
       <div className="row">
-        <form className="col s12">
+        <form className="col s12" onKeyDownCapture ={this.onKeyPress}>
           <div className="row">
             <div className="input-field col s12 m6">
               <input
@@ -103,10 +135,11 @@ class UserForm extends Component {
               <input
                 name="interest"
                 type="text"
+                onKeyUpCapture ={this.addInterest}
                 value={this.state.interest}
                 onChange={this.handleInputChange}
               />
-              <label htmlFor="Interest">Interest</label>
+              <label htmlFor="Interest">Interest (Press enter to Add)</label>
             </div>
           </div>
           <div className="row">
@@ -114,10 +147,11 @@ class UserForm extends Component {
               <input
                 name="knowledge"
                 type="text"
+                onKeyUpCapture ={this.addKnowledge}
                 value={this.state.knowledge}
                 onChange={this.handleInputChange}
               />
-              <label htmlFor="knowledge">Knowledge</label>
+              <label htmlFor="knowledge">Knowledge (Press enter to Add)</label>
             </div>
           </div>
           <div className="row">
